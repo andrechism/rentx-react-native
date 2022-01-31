75e2c6d2bf269ad2594faa85372e4684063d2318
André Aragão <andre.correia@agenciaeplus.com.br>
2022-01-31 19:41:48 -0300

    added typescript and styled-components

---
+++ App.tsx
@@ -0,0 +1,54 @@
+import { StatusBar } from 'expo-status-bar';
+
+import AppLoading from 'expo-app-loading'
+
+import { ThemeProvider } from 'styled-components/native';
+import theme from './src/styles/theme';
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium
+} from '@expo-google-fonts/inter';
+
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold
+} from '@expo-google-fonts/archivo';
+
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Home } from './src/screens/Home';
+
+export default function App() {
+  const [fontsloaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold
+  })
+
+  if (!fontsloaded) {
+    return <AppLoading />
+  }
+
+  return (
+    <View style={styles.container}>
+      <StatusBar style="auto" />
+      <ThemeProvider theme={theme}>
+        <Home />
+      </ThemeProvider>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
