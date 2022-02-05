f1ca76a6948e203aef5ab9c87f374d49af58b6c4
André Aragão <andre.correia@agenciaeplus.com.br>
2022-02-04 23:00:36 -0300

    header finished

--- App.tsx
+++ App.tsx
@@ -35,20 +35,8 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
       <ThemeProvider theme={theme}>
         <Home />
       </ThemeProvider>
-    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
